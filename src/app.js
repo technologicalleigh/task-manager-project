c81ef08b42eaea1693f26aa6226d1998ff67ab80
technologicalleigh <50113696+technologicalleigh@users.noreply.github.com>
2021-12-13 15:14:35 -0600

    color pallet + mongodb
    added color pallets stuff and started adding mongodb

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const date = require('../modules/date');
 const express = require("express");
+const bodyParser= require('body-parser') 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}))
+//The urlencoded method within body-parser tells body-parser to extract data from the <form> element and add them to the body property in the request object.
 
 app.set("view engine", "ejs");
 
